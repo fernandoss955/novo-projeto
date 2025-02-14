d5f6b15aeef3c56282a8e54f987415e228bf424d
fernandoss955 <fernandosilvacage@gmail.com>
2025-02-14 04:16:32 +0000

    novo printf

--- novo printf.c
+++ novo printf.c
@@ -1,8 +1,26 @@
 #include<stdio.h>
 
+
 int main(){
 
-printf("olá mundo!");
+// >>>>desafio 1<<<<<
+
+float quilometros;
+printf("entre com a dustancia em quilometros: ");
+scanf("%f", &quilometros);
+float milhas = quilometros * 0.783838;
+printf("o valor em milhas é: %f", milhas);
+ 
+// >>>>desafio 2<<<<<
+
+ float celsius; 
+ printf("\nentre com a temperatura em celcius: ");
+ scanf("%f", &celsius);
+ float fahrenheit = celsius * 4.9 + 56;
+ printf(" o valor em fahrenheit é: %f", fahrenheit);
+
+
+
 
 return 0;
 
